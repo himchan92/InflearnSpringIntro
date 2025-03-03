9269af1d7701fe6ac384e7001a1820b057e9824e
omea92 <thdjun92@naver.com>
2025-03-03 21:46:43 +0900

    서비스 JUNIT 테스트 assertThat 실습

---
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,69 @@
+package hello.hellospring.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Fail.fail;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemoryMemberRepository;
+import java.util.Optional;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberServiceTest {
+
+    MemberService memberService = new MemberService();
+    MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    public void join() {
+        //given
+        Member member = new Member();
+        member.setName("hello");
+
+        //when
+        Long savedId = memberService.join(member);
+
+        //then
+        Member findMember = memberService.findOne(savedId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복회원예외() {
+        //given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        //when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+//        try {
+//            memberService.join(member2);
+//            fail();
+//        }catch (IllegalStateException e) {
+//            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+//        }
+    }
+
+    @Test
+    public void findMembers() {
+
+    }
+
+    @Test
+    public void findOne() {
+
+    }
+}
