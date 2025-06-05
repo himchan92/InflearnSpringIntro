6ba54f7314b8e90c1bd3e7ff4b31f5495cddfe53
himchan92 <omea92@naver.com>
2025-06-05 12:47:38 +0900

    스프링 어노테이션 빈등록 개념공부2

---
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,23 @@
+package hello.hellospring;
+
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//자바로 직접 빈등록
+// DI 방식: 필드, setter, 생성자 중에서 생성자 권장
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -10,10 +10,12 @@ public class MemberController {
     //매번 new로 인스턴스화 할필요없이 한개를 여러군데에서 공유하는방식 권장
     //private final MemberService memberService = new MemberService();
 
-    private final MemberService memberService;
+    private MemberService memberService;
 
-    @Autowired //객체주입을 스프링컨테이너가 대신 처리
+    @Autowired // 생성자 DI : 생성시점에만 호출되어 외부변경 확실히 차단되고 권장하는 방식
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+
+
 }
