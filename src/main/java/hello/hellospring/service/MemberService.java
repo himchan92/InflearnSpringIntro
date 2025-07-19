ee8aef33e7d114aac1a55115860aff8dee12d92f
omea92 <thdjun92@naver.com>
2025-07-20 01:09:30 +0900

    컴포넌트 자동스캔 공부

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -4,6 +4,7 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service //순수 자바코드는 스프링컨테이너가 인식하지 못해 명시해줘서 관리하게 명시하여 매핑
@@ -11,6 +12,7 @@ public class MemberService {
 
   private final MemberRepository memberRepository;
 
+  @Autowired //스프링컨테이너가 대신 생성자 주입 지원(사전 어노테이션 매핑으로 컨테이너 인식 필수)
   public MemberService(MemberRepository memberRepository) {
     this.memberRepository = memberRepository;
   }
